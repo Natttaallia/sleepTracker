3f65aa685cff8dce520926ea2c4f297e1fa7c5bd
Kulbaka Nataly <kulbacka.natalya@gmail.com>
2021-08-16 14:34:23 +0300

    simple rv

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/TextItemViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author Kulbaka Nataly
+ * @date 16.08.2021
+ */
+class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
